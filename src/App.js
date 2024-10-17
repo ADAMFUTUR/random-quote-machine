dd797f999da45a16ccd34460772b4c50f6441db8
ADAMFUTUR <IMLOULADAM8@GMAIL.COM>
2024-10-17 22:56:21 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const quotes = [
+  {
+    quote: "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote: "Life is 10% what happens to us and 90% how we react to it.",
+    author: "Charles R. Swindoll",
+  },
+  {
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+];
 
 function App() {
+  const [quote, setQuote] = useState(quotes[0].quote);
+  const [author, setAuthor] = useState(quotes[0].author);
+
+  const getNewQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const newQuote = quotes[randomIndex];
+    setQuote(newQuote.quote);
+    setAuthor(newQuote.author);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box" className="quote-box">
+      <h1 id="text">{quote}</h1>
+      <p id="author">- {author}</p>
+      <button id="new-quote" onClick={getNewQuote}>
+        New Quote
+      </button>
+      <a
+        id="tweet-quote"
+        href={`https://twitter.com/intent/tweet?text="${quote}" - ${author}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Tweet
+      </a>
     </div>
   );
 }
